a405fc8e7c52e945743f2c5f052f5779f2745b80
jeishu <jeremy.zhu@utexas.edu>
2021-03-01 17:39:36 -0600

    Changed tab icon

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,8 @@ const FILES_TO_CACHE = [
   "/index.js",
   "/favicon.ico", 
   "/styles.css", 
-  "/icons/icon-192x192.png", 
+  "/icons/icon-128x128.png", 
+  "/icons/icon-256x256.png",
   "/icons/icon-512x512.png"
 ];
   
